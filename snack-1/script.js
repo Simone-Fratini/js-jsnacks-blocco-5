6ac5f41bab9d962b6f450a8830bb60933baf2d0d
Simone-Fratini <simone@fratini.com>
2024-11-15 17:02:08 +0100

    0 to 4 done

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for (name of names){
+    console.log(name);
+}
